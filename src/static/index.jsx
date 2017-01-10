06829a0b4773dbce17caf92b2f15e6a82707364e
Deepak Kannan <kannan.deepak@gmail.com>
2017-01-10 17:40:53 +0530

    Add react-router with hashHistory and api calls using redux-thunk
    Add react-router and a bare-bones route.
      - /      => existing app
      - /about => a one-liner about-us text
    
    because we are loading a url in Electron
    we need to use hashHistory and not browserHistory
    
    Also fetch some dummy data from the jsonplaceholder service,
    using the react-router onEnter hook.
    use it to populate the list of todos.

--- src/static/index.jsx
+++ src/static/index.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
 
 import todoApp from '../react-app/reducers';
 import App from '../react-app/components/App.jsx';
+import About from '../react-app/components/About.jsx';
+import { dispatchTodos, addTodo } from '../react-app/actions';
 
-let store = createStore(todoApp);
+const initialState = {};
+const enhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+const store = createStore(
+  todoApp,
+  initialState,
+  enhancers
+);
+
+// called only once,
+// before application initially starts to render react-route and any of its related DOM elements
+function onAppInit(dispatch) {
+  return (nextState, replace, callback) => {
+    dispatch(dispatchTodos())
+      .then((todos) => {
+        todos.data.map(t => dispatch(addTodo(t.title)));
+        // callback is like a "next" function, app initialization is stopped until it is called.
+        callback();
+      });
+  };
+}
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path="/about" component={About}/>
+      <Route path="/" component={App} onEnter={onAppInit(store.dispatch)}/>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
